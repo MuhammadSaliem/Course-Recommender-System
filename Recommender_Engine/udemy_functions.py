280b5afd74b829a0f73514e2780870e2e9188eb6
Saleem <mohamedselim701@gmail.com>
2022-07-08 10:41:36 +0200

    Shorthand another line of code in the recommendation function

--- Recommender_Engine/udemy_functions.py
+++ Recommender_Engine/udemy_functions.py
@@ -10,13 +10,13 @@ def similar_course_list(course_id, n_courses, df_courses, df_norm):
     df_result= df_result.sort_values('cosine_similarity', ascending=False).reset_index(drop=True)
     return title, df_result.iloc[1:n_courses+1][['published_title', 'cosine_similarity','image', 'instructor', 'price']]
 
-def recommend_for_user(user_name, n_courses, taken_courses, df_courses, df_norm):
+def recommend_for_user(n_courses, taken_courses, df_courses, df_norm):
     n_courses += 1
     #list_courses= taken_courses
-    numOfCourses =len(taken_courses)
+    #numOfCourses =len(taken_courses)
     index_courses=df_courses[df_courses['id'].isin(taken_courses)].index
 
-    if numOfCourses>0:
+    if len(taken_courses) >= 1:
         #n_courses=n_courses+1
         tmp_dataFrame =df_courses.copy()
         for i, course_id in enumerate(taken_courses):
@@ -25,9 +25,9 @@ def recommend_for_user(user_name, n_courses, taken_courses, df_courses, df_norm)
             #Y = df_norm.loc[id_].values.reshape(1, -1)
             tmp_dataFrame[i] = cosine_similarity(df_norm.values, df_norm.loc[df_courses[df_courses['id']==course_id].index.values].values.reshape(1, -1))
 
-        tmp_dataFrame['avg_cos_sim']= tmp_dataFrame.iloc[:,-numOfCourses:].mean(axis=1).values
+        tmp_dataFrame['avg_cos_sim']= tmp_dataFrame.iloc[:,-len(taken_courses):].mean(axis=1).values
         #df_temp['avg_cos_sim']=temp_avg
-        tmp_dataFrame.drop(index=index_courses, inplace=True)
+        tmp_dataFrame.drop(index=df_courses[df_courses['id'].isin(taken_courses)].index, inplace=True)
         tmp_dataFrame =tmp_dataFrame.sort_values('avg_cos_sim', ascending=False).reset_index(drop=True)
         return tmp_dataFrame #return df_temp for Flask API
 
