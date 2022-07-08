57213a8846bdf24565e5086e4810c79babb283e2
Saleem <mohamedselim701@gmail.com>
2022-07-08 10:26:48 +0200

    Shorthand course_recommend function

--- Recommender_Engine/udemy_functions.py
+++ Recommender_Engine/udemy_functions.py
@@ -1,25 +1,21 @@
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def recommend_courses(course_id, n_courses, df_courses, df_norm):
-    n_courses=n_courses+1
+def similar_course_list(course_id, n_courses, df_courses, df_norm):
     id_=df_courses[df_courses['id']==course_id].index.values
     title=df_courses[df_courses['id']==course_id]['published_title']
-    X = df_norm.values
-    Y = df_norm.loc[id_].values.reshape(1, -1)
-    cos_sim = cosine_similarity(X, Y)
-    df_sorted=df_courses.copy()
-    df_sorted['cosine_similarity'] = cos_sim
-    df_sorted=df_sorted.sort_values('cosine_similarity', ascending=False).reset_index(drop=True)
+    df_result= df_courses.copy()
 
-    return title, df_sorted.iloc[1:n_courses][['published_title', 'cosine_similarity','image', 'instructor', 'price']]
+    df_result['cosine_similarity'] = cosine_similarity(df_norm.values, df_norm.loc[id_].values.reshape(1, -1))  
+    df_result= df_result.sort_values('cosine_similarity', ascending=False).reset_index(drop=True)
+    return title, df_result.iloc[1:n_courses+1][['published_title', 'cosine_similarity','image', 'instructor', 'price']]
 
 def recommend_for_user(user_name, n_courses, taken_courses, df_courses, df_norm):
     list_courses= taken_courses
     len_courses=len(list_courses)
     index_courses=df_courses[df_courses['id'].isin(list_courses)].index
     for course_id in list_courses:
-        title, df_recommend= recommend_courses(course_id, n_courses, df_courses, df_norm)
+        title, df_recommend= similar_course_list(course_id, n_courses, df_courses, df_norm)
         print('The following courses are recommended after taking the course {} with the id {}:'
           .format(title.values[0],course_id))
         print(df_recommend)
